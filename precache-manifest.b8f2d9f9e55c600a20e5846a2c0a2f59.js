15c3bb58583ad2524fc74875c00b2525ebb0c3d2
Mario Gascón Viúdez <mariogv1994@gmail.com>
2019-10-15 21:10:29 +0200

    Updates

---
+++ precache-manifest.b8f2d9f9e55c600a20e5846a2c0a2f59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "445e8768773fbb1ac9d73d7164cc7945",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "555965cbe28b8b46e8a7",
+    "url": "/my-app/static/css/main.8c53da67.chunk.css"
+  },
+  {
+    "revision": "e5689f98285ca85ac887",
+    "url": "/my-app/static/js/2.84ed3b1e.chunk.js"
+  },
+  {
+    "revision": "555965cbe28b8b46e8a7",
+    "url": "/my-app/static/js/main.294361ba.chunk.js"
+  },
+  {
+    "revision": "247eec36cf986955c323",
+    "url": "/my-app/static/js/runtime-main.fe96fb05.js"
+  }
+]);
